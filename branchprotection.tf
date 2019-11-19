d4a14747793b873997486529a1f4f7a661464cbf
Grant Hewitt <grant_hewitt_1@hotmail.co.uk>
2019-11-19 12:44:45 +0100

    Add branch protection requiring one approver

---
+++ branchprotection.tf
@@ -0,0 +1,67 @@
+resource "github_branch_protection" "branch_protection_api_gateway" {
+  repository     = "ApiGateway"
+  branch         = "master"
+  enforce_admins = true
+
+  required_status_checks {
+    strict   = false
+    contexts = ["build"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    require_code_owner_reviews      = true
+    required_approving_review_count = 1
+  }
+}
+
+resource "github_branch_protection" "branch_protection_session_service" {
+  repository     = "SessionService"
+  branch         = "master"
+  enforce_admins = true
+
+  required_status_checks {
+    strict   = false
+    contexts = ["build"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    require_code_owner_reviews      = true
+    required_approving_review_count = 1
+  }
+}
+
+resource "github_branch_protection" "branch_protection_conference_session_service" {
+  repository     = "ConferenceSessionService"
+  branch         = "master"
+  enforce_admins = true
+
+  required_status_checks {
+    strict   = false
+    contexts = ["build"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    require_code_owner_reviews      = true
+    required_approving_review_count = 1
+  }
+}
+
+resource "github_branch_protection" "branch_protection_feed_service" {
+  repository     = "FeedService"
+  branch         = "master"
+  enforce_admins = true
+
+  required_status_checks {
+    strict   = false
+    contexts = ["build"]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    require_code_owner_reviews      = true
+    required_approving_review_count = 1
+  }
+}
