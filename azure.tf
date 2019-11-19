3fb86eb5d37526e7980a62cfbd129d72e2468aa3
Oliver <olwheele@microsoft.com>
2019-11-19 19:38:40 +0100

    Add azure container registry

---
+++ azure.tf
@@ -0,0 +1,19 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=1.36.1"
+}
+
+
+resource "azurerm_resource_group" "ngc" {
+  name     = "ngc"
+  location = "North Europe"
+}
+
+resource "azurerm_container_registry" "ngc" {
+  name                = "ngc"
+  sku                 = "Standard"
+  admin_enabled       = false
+  resource_group_name = azurerm_resource_group.ngc.name
+  location            = azurerm_resource_group.ngc.location
+}
