226293d43363744ac998214d55b9d01d5046c0b5
Simon Lindblad <simon.lindblad93@gmail.com>
2019-11-20 18:10:23 +0100

    Fix

--- azure.tf
+++ azure.tf
@@ -65,11 +65,11 @@ resource "azurerm_cosmosdb_mongo_database" "ngc_sessions" {
   account_name        = azurerm_cosmosdb_account.ngc.name
 }
 
-resource "azurerm_cosmosdb_mongo_collection" "example" {
+resource "azurerm_cosmosdb_mongo_collection" "ngc" {
   name                = "Sessions"
   resource_group_name = azurerm_resource_group.ngc.name
   account_name        = azurerm_cosmosdb_account.ngc.name
-  database_name       = azurerm_cosmosdb_sessions_mongo_database.ngc.name
+  database_name       = azurerm_cosmosdb_mongo_database.ngc.name
 
   default_ttl_seconds = "-1"
   shard_key           = "_id"
