fa31e386df7d44b1ee4dc51bebc9fe1288b2d776
Simon Lindblad <simon.lindblad93@gmail.com>
2019-11-20 17:53:22 +0100

    Add mongo db kube config

--- azure.tf
+++ azure.tf
@@ -4,6 +4,11 @@ provider "azurerm" {
   version = "=1.36.1"
 }
 
+data "azurerm_location" "ngc" {
+  primary_location = "North Europe"
+  secondary_location = "West Europe"
+}
+
 resource "azurerm_resource_group" "ngc" {
   name     = "ngc"
   location = "North Europe"
@@ -14,7 +19,7 @@ resource "azurerm_container_registry" "ngc" {
   sku                 = "Standard"
   admin_enabled       = false
   resource_group_name = azurerm_resource_group.ngc.name
-  location            = azurerm_resource_group.ngc.location
+  location            = data.azurerm_location.ngc.primary_location
 }
 
 variable "cluster_client_secret" {
@@ -38,4 +43,39 @@ resource "azurerm_kubernetes_cluster" "ngc" {
     client_id     = "5a6f663a-b005-4f4b-88af-ad504dbacd8b"
     client_secret = var.cluster_client_secret
   }
+}
+
+resource "azurerm_cosmosdb_account" "ngc" {
+  name                = "nextgenconf"
+  location            = azurerm_resource_group.ngc.location
+  resource_group_name = azurerm_resource_group.ngc.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+
+  geo_location {
+    location          = data.azurerm_location.ngc.secondary_location
+    failover_priority = 1
+  }
+}
+
+resource "azurerm_cosmosdb_sessions_mongo_database" "ngc" {
+  name                = "SessionsDb"
+  resource_group_name = azurerm_resource_group.ngc.name
+  account_name        = azurerm_cosmosdb_account.ngc.name
+}
+
+resource "azurerm_cosmosdb_mongo_collection" "example" {
+  name                = "Sessions"
+  resource_group_name = azurerm_resource_group.ngc.name
+  account_name        = azurerm_cosmosdb_account.ngc.name
+  database_name       = azurerm_cosmosdb_sessions_mongo_database.ngc.name
+
+  default_ttl_seconds = "-1"
+  shard_key           = "_id"
 }
