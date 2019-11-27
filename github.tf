ae876a96e33ca5d66b9f38ba52fa32e32fa25cb6
Oliver <olwheele@microsoft.com>
2019-11-27 10:16:55 +0100

    apply terraform fmt

--- github.tf
+++ github.tf
@@ -3,45 +3,45 @@ provider "github" {
 }
 
 resource "github_repository" "repository_k8s_config" {
-  name               = "KubeConfig"
-  description        = "Repository containing the kubernetes resource files for the NextGenConf cluster"
-  has_issues         = true
-  has_projects       = false
+  name         = "KubeConfig"
+  description  = "Repository containing the kubernetes resource files for the NextGenConf cluster"
+  has_issues   = true
+  has_projects = false
 }
 
 resource "github_repository" "repository_conference_service" {
-  name               = "ConferenceService"
-  description        = "Microservice for storing conferences"
-  has_issues         = true
-  has_projects       = false
+  name         = "ConferenceService"
+  description  = "Microservice for storing conferences"
+  has_issues   = true
+  has_projects = false
 }
 
 resource "github_repository" "repository_web_app" {
-  name               = "WebApp"
-  description        = "React app displaying conferences"
-  has_issues         = true
-  has_projects       = false
+  name         = "WebApp"
+  description  = "React app displaying conferences"
+  has_issues   = true
+  has_projects = false
 }
 
 resource "github_repository" "repository_api_gateway" {
-  name               = "ApiGateway"
-  description        = "Microservice for aggregating requests to storage services"
-  has_issues         = true
-  has_projects       = false
+  name         = "ApiGateway"
+  description  = "Microservice for aggregating requests to storage services"
+  has_issues   = true
+  has_projects = false
 }
 
 resource "github_repository" "repository_session_service" {
-  name               = "SessionService"
-  description        = "Microservice for storing conference sessions"
-  has_issues         = true
-  has_projects       = false
+  name         = "SessionService"
+  description  = "Microservice for storing conference sessions"
+  has_issues   = true
+  has_projects = false
 }
 
 resource "github_repository" "repository_conference_session_service" {
-  name               = "ConferenceSessionService"
-  description        = "Microservice for storing the relationship between conferences and sessions"
-  has_issues         = true
-  has_projects       = false
+  name         = "ConferenceSessionService"
+  description  = "Microservice for storing the relationship between conferences and sessions"
+  has_issues   = true
+  has_projects = false
 }
 
 resource "github_repository" "repository_feed_service" {
